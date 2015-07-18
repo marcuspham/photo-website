80aa846b0efa172028fac049f8e7f0b7a110b4a0
Marcus Pham <marcustanpham@gmail.com>
2015-07-17 21:53:00 -0700

    start leaf animation

--- js/leaf.js
+++ js/leaf.js
@@ -8,8 +8,21 @@ function reveal() {
 	var leaf1 = $('#leaf1');
 	var leaf2 = $('#leaf2');
 
-	leaf1.velocity({ left: -100 }, 1000);
+	for (var i = 0; i < 5; i++) {
+		var leaf = $(document.createElement('img')).attr('src', 'http://pngimg.com/upload/green_leaves_PNG3678.png');
+		leaf.addClass('leaf');
+		leaf.css('top', 100 * i);
 
-	leaf2.velocity({ right: -100 }, 1000);
+		leaf.appendTo('#container');
+	}
+
+	$('.leaf')
+		.velocity("transition.slideRightBigOut", {
+			stagger: 100,
+			drag: true,
+			backwards: true
+		})
+
+		.delay(1000);
 
 }
