bd6e5ed07e3496b0ec80af5c528baf53194ba8a8
Tisha Trongtham <ttseattle@yahoo.com>
2015-07-19 22:54:38 -0700

    Updated menu and contact page some more

---
+++ js/contact.js
@@ -0,0 +1,16 @@
+var myApp = angular.module('myApp', [])
+myApp.controller('myCtrl', function($scope){
+  $scope.master = {}
+  $scope.user = {}
+  $scope.class = "glyphicon-send";
+  $scope.sent = false;
+  $scope.submit = function() {
+  	$scope.class = "glyphicon-ok";
+  	$scope.sent = true;
+  };
+  $scope.reset = function() {
+    $scope.user = angular.copy($scope.master);
+    if ($scope.form) $scope.form.$setPristine();
+  };
+})
+
